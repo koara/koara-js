740a42cad3f541ea1afa4602a6a6717bdbcfdeb4
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-02-03 07:40:57 +0100

    start charstream

--- koara.js
+++ koara.js
@@ -3,19 +3,19 @@
 (function(exports) {
 	
 	var StringReader = function(text) {
+		var index = 0;
 		return {
-			index: 0,
 			read: function(buffer, offset, length) {
-				if(text.substr(this.index).length > 0) {
+				if(text.substr(index).length > 0) {
 					var charactersRead = 0;
 					for(var i=0; i < length; i++) {
-						var c = text.substr((this.index + i), 1);
+						var c = text.substr(index + i, 1);
 						if(c !== '') {
 							buffer[offset + i] = c;
 							charactersRead++;
 						}
 					}
-					this.index += length;
+					index += length;
 					return charactersRead;
 				}
 				return -1;
@@ -23,5 +23,43 @@
 		}
 	}
 	
+	var CharStream = function(reader) {
+		var available = 4096;
+		var bufsize = 4096;
+		var tokenBegin;
+		var bufcolumn = [];
+		var bufpos = -1;
+		var bufline = [];
+		var column = 0;
+		var line = 1;
+		var prevCharIsLF;
+		var reader;
+		var buffer = [];
+		var maxNextCharInd = 0;
+		var inBuf = 0;
+		var tabSize = 4;
+		return {
+			beginToken: function() {
+				return 'a'
+			},
+			readChar: function() {
+				return 'a'
+			},
+			getBeginColumn: function() {
+				return 1
+			},
+			getBeginLine: function() {
+				return 1
+			},
+			getEndColumn: function() {
+				return 1
+			},
+			getEndLine: function() {
+				return 1
+			}
+		}
+	}
+	
+	exports.CharStream = CharStream;
 	exports.StringReader = StringReader;
 })(typeof exports === 'undefined' ? this.koara = {} : exports);
