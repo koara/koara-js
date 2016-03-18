e3fe79ccde896faff5b1e7f08f70d2478945fd1f
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-03-18 07:19:26 +0100

    fix linting issues

--- dist/koara.js
+++ dist/koara.js
@@ -12,7 +12,8 @@ var used = []
   , exports = module.exports = {};
 
 exports.Parser = require('./koara/parser');
-},{"./koara/parser":22}],3:[function(require,module,exports){
+exports.Html5Renderer = require('./koara/html5renderer');
+},{"./koara/html5renderer":20,"./koara/parser":23}],3:[function(require,module,exports){
 "use strict";
 
 var Node = require('./node');
@@ -451,6 +452,202 @@ module.exports = CharStream;
 },{}],20:[function(require,module,exports){
 "use strict";
 
+var ListItem = require('./ast/listitem');
+
+function Html5Renderer() {
+	this.out = "";
+}
+
+Html5Renderer.prototype = {
+	constructor: Html5Renderer,
+
+	visitDocument: function(node) {
+		this.out = "";
+		this.level = 0;
+		this.listSequence = [];
+		node.childrenAccept(this);
+	},
+
+	visitHeading: function(node) {
+		this.out += this.indent() + "<h" + 	node.value + ">";
+		node.childrenAccept(this);
+		this.out += "</h" + node.value + ">\n";
+		if (!node.isNested()) {
+            this.out += "\n";
+        }
+	},
+
+	visitBlockQuote: function(node) {
+		this.out += this.indent() + "<blockquote>";
+		if (node.children && node.children.length > 0) {
+            this.out += "\n";
+		}
+        this.level++;
+        node.childrenAccept(this);
+        this.level--;
+        this.out += this.indent() + "</blockquote>\n";
+        if (!node.isNested()) {
+           this.out += "\n";
+        }
+	},
+
+	visitListBlock: function(node) {
+		this.listSequence.push(0);
+		var tag = node.ordered ? "ol" : "ul";
+
+		this.out += this.indent() + "<" + tag + ">\n";
+		this.level++;
+		node.childrenAccept(this);
+		this.level--;
+		this.out += this.indent() + "</" + tag + ">\n";
+		if (!node.isNested()) {
+            this.out += "\n";
+		}
+		this.listSequence.pop();
+	},
+
+	visitListItem: function(node) {
+		var seq = Number(this.listSequence[this.listSequence.length - 1]) + 1;
+
+		this.listSequence[this.listSequence.length - 1] = seq;
+
+        this.out += this.indent() + "<li";
+		if (node.number && (seq !== Number(node.number))) {
+			this.out += " value=\"" + node.number + "\"";
+			this.listSequence.push(node.number);
+		}
+		this.out += ">";
+		if (node.children && node.children.length > 0) {
+			var block = (node.children[0].constructor.name === "Paragraph" || node.children[0].constructor.name === "BlockElement");
+
+			if (node.children.length > 1 || !block) {
+                this.out += "\n";
+            }
+			this.level++;
+			node.childrenAccept(this);
+			this.level--;
+			if (node.children.length > 1 || !block) {
+				this.out += this.indent();
+			}
+		}
+		this.out += "</li>\n";
+	},
+
+	visitCodeBlock: function(node) {
+		this.out += this.indent() + "<pre><code";
+		if (node.language) {
+			this.out += " class=\"language-" + this.escape(node.language) + "\"";
+		}
+		this.out += ">";
+		this.out += this.escape(node.value) + "</code></pre>\n";
+		if (!node.isNested()) {
+            this.out += "\n";
+        }
+	},
+
+	visitParagraph: function(node) {
+		if (node.isNested() && (node.parent instanceof ListItem) && node.isSingleChild()) {
+			node.childrenAccept(this);
+		} else {
+			this.out += this.indent() + "<p>";
+			node.childrenAccept(this);
+			this.out += "</p>\n";
+			if (!node.isNested()) {
+				this.out += "\n";
+			}
+		}
+	},
+//
+//	@Override
+//	public void visit(BlockElement node) {
+//		if(node.isNested() && (node.getParent() instanceof ListItem) && node.isSingleChild()) {
+//			node.childrenAccept(this);
+//		} else {
+//			out.append(indent());
+//			node.childrenAccept(this);
+//			if(!node.isNested()) { out.append("\n"); }
+//		}
+//	}
+//
+	visitImage: function(node) {
+		this.out += "<img src=\"" + this.escapeUrl(node.value) + "\" alt=\"";
+		node.childrenAccept(this);
+		this.out += "\" />";
+	},
+
+	visitLink: function(node) {
+		this.out += "<a href=\"" + this.escapeUrl(node.value.toString()) + "\">";
+		node.childrenAccept(this);
+		this.out += "</a>";
+	},
+
+	visitStrong: function(node) {
+		this.out += "<strong>";
+		node.childrenAccept(this);
+		this.out += "</strong>";
+	},
+
+	visitEm: function(node) {
+		this.out += "<em>";
+		node.childrenAccept(this);
+		this.out += "</em>";
+	},
+
+	visitCode: function(node) {
+		this.out += "<code>";
+		node.childrenAccept(this);
+		this.out += "</code>";
+	},
+
+	visitText: function(node) {
+		this.out += this.escape(node.value);
+	},
+
+	visitLineBreak: function(node) {
+		this.out += "<br>\n" + this.indent();
+		node.childrenAccept(this);
+	},
+
+	escapeUrl: function(text) {
+        return text.replace(/ /gm, "%20").
+            replace(/\"/gm, "%22").
+            replace(/`/gm, "%60").
+            replace(/</gm, "%3C").
+            replace(/>/gm, "%3E").
+            replace(/\[/gm, "%5B").
+            replace(/\]/gm, "%5D").
+            replace(/\\/gm, "%5C");
+	},
+
+	indent: function() {
+		var repeat = this.level * 2;
+        var ind = "";
+
+		for (var i = repeat - 1; i >= 0; i--) {
+			ind += " ";
+		}
+		return ind;
+	},
+
+	escape: function(text) {
+		return text.replace(/&/gm, "&amp;").
+            replace(/</gm, "&lt;").
+            replace(/>/gm, "&gt;").
+            replace(/\"/gm, "&quot;");
+	},
+
+	getOutput: function() {
+        return this.out.trim();
+	}
+
+};
+
+module.exports = Html5Renderer;
+
+
+},{"./ast/listitem":14}],21:[function(require,module,exports){
+"use strict";
+
 function StringReader(text) {
 	this.index = 0;
 	this.text = text;
@@ -481,14 +678,14 @@ StringReader.prototype = {
 
 module.exports = StringReader;
 
-},{}],21:[function(require,module,exports){
+},{}],22:[function(require,module,exports){
 "use strict";
 
 function LookaheadSuccess() {};
 
 module.exports = LookaheadSuccess;
 
-},{}],22:[function(require,module,exports){
+},{}],23:[function(require,module,exports){
 "use strict";
 
 var LookaheadSuccess = require('./lookaheadsuccess');
@@ -3225,7 +3422,7 @@ Parser.prototype = {
     },
 
     consumeToken: function(kind) {
-        old = this.token;
+        var old = this.token;
         if (this.token.next !== null) {
             this.token = this.token.next;
         } else {
@@ -3240,7 +3437,7 @@ Parser.prototype = {
     },
 
     getToken: function(index) {
-        t = this.lookingAhead ? this.scanPosition : this.token;
+        var t = this.lookingAhead ? this.scanPosition : this.token;
         for (var i = 0; i < index; i++) {
             if (t.next) {
                 t = t.next;
@@ -3255,7 +3452,7 @@ Parser.prototype = {
 
 module.exports = Parser;
 
-},{"./ast/Document":3,"./ast/blockquote":5,"./ast/code":6,"./ast/codeblock":7,"./ast/em":8,"./ast/heading":9,"./ast/image":10,"./ast/linebreak":11,"./ast/link":12,"./ast/listblock":13,"./ast/listitem":14,"./ast/paragraph":16,"./ast/strong":17,"./ast/text":18,"./charstream":19,"./io/stringreader":20,"./lookaheadsuccess":21,"./token":23,"./tokenmanager":24,"./treestate":25}],23:[function(require,module,exports){
+},{"./ast/Document":3,"./ast/blockquote":5,"./ast/code":6,"./ast/codeblock":7,"./ast/em":8,"./ast/heading":9,"./ast/image":10,"./ast/linebreak":11,"./ast/link":12,"./ast/listblock":13,"./ast/listitem":14,"./ast/paragraph":16,"./ast/strong":17,"./ast/text":18,"./charstream":19,"./io/stringreader":21,"./lookaheadsuccess":22,"./token":24,"./tokenmanager":25,"./treestate":26}],24:[function(require,module,exports){
 "use strict";
 
 function Token(kind, beginLine, beginColumn, endLine, endColumn, image) {
@@ -3269,8 +3466,10 @@ function Token(kind, beginLine, beginColumn, endLine, endColumn, image) {
 
 module.exports = Token;
 
-},{}],24:[function(require,module,exports){
+},{}],25:[function(require,module,exports){
 "use strict";
+
+var Token = require('./token');
 
 function TokenManager(stream) {
 	this.cs = stream;
@@ -3313,6 +3512,7 @@ TokenManager.prototype = {
                 try {
                     this.curChar = this.cs.beginToken();
                 } catch (e) {
+                	console.log(e)
                     this.matchedKind = 0;
                     this.matchedPos = -1;
                     return this.fillToken();
@@ -3330,6 +3530,7 @@ TokenManager.prototype = {
                 }
             }
         } catch (e) {
+        	console.log(e)
             return null;
         }
     },
@@ -3369,6 +3570,7 @@ TokenManager.prototype = {
         try {
             this.curChar = this.cs.readChar();
         } catch (e) {
+        	console.log(e)
             return pos + 1;
         }
         return this.moveNfa(state, pos + 1);
@@ -3571,6 +3773,7 @@ TokenManager.prototype = {
             try {
                 this.curChar = this.cs.readChar();
             } catch (e) {
+            	console.log(e)
                 return curPos;
             }
           }
@@ -3643,7 +3846,7 @@ TokenManager.prototype = {
 module.exports = TokenManager;
 
 
-},{}],25:[function(require,module,exports){
+},{"./token":24}],26:[function(require,module,exports){
 "use strict";
 
 function TreeState() {
@@ -3666,7 +3869,8 @@ TreeState.prototype = {
 
 		this.currentMark = this.marks.pop();
 		while (a-- > 0) {
-          c = this.popNode();
+          var c = this.popNode();
+          
           c.parent = n;
           n.add(c, a);
         }
