f63b86c957ca7aec10d31fb532cdfe62d6295caa
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-03-20 18:41:42 +0100

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -4,29 +4,23 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var buffer = require('vinyl-buffer');
 var jasmine = require('gulp-jasmine');
-var gutil = require('gulp-util');
 var eslint = require('gulp-eslint');
-var karma = require('karma');
 var uglify = require('gulp-uglify');
 var reactify = require('reactify');
 var rename = require('gulp-rename');
 var source = require('vinyl-source-stream');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('default', ['lint', 'scripts', 'test']);
+gulp.task('default', ['lint', 'bundle', 'test']);
 
-gulp.task('scripts', function() {
-	var b = browserify({
-	    entries: './index.js',
-	    debug: true,
-	    transform: [reactify]
-	});
-
+gulp.task('bundle', function() {
+	var b = browserify({ entries: './index.js', debug: true, transform: [reactify] });
 	return b.bundle()
 	    .pipe(source('index.js'))
+	    .pipe(rename('koara.js'))
 	    .pipe(buffer())
 	    .pipe(gulp.dest('dist'))
-	    .pipe(rename('index.min.js'))
+	    .pipe(rename('koara.min.js'))
 	    .pipe(sourcemaps.init({loadMaps: true}))
 	    .pipe(uglify())
 	    .pipe(sourcemaps.write('.'))
