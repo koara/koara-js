668b603201b86a14e9165b44acf7dd1dc82cde5c
Andy Van Den Heuvel <andy.vandenheuvel@gmail.com>
2016-03-29 16:35:19 +0200

    test with coverage

--- gulpfile.js
+++ gulpfile.js
@@ -17,17 +17,17 @@ var webserver = require('gulp-webserver');
 gulp.task('default', ['lint', 'bundle', 'test']);
 
 gulp.task('bundle', function() {
-	var b = browserify({ entries: './index.js', standalone: 'koara', debug: true, transform: [reactify] });
-	return b.bundle()
-	    .pipe(source('index.js'))
-	    .pipe(rename('koara.js'))
-	    .pipe(buffer())
-	    .pipe(gulp.dest('dist'))
-	    .pipe(rename('koara.min.js'))
-	    .pipe(sourcemaps.init({loadMaps: true}))
-	    .pipe(uglify())
-	    .pipe(sourcemaps.write('.'))
-	    .pipe(gulp.dest('dist'));
+  var b = browserify({ entries: './index.js', standalone: 'koara', debug: true, transform: [reactify] });
+  return b.bundle()
+    .pipe(source('index.js'))
+    .pipe(rename('koara.js'))
+    .pipe(buffer())
+    .pipe(gulp.dest('dist'))
+    .pipe(rename('koara.min.js'))
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(uglify())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist'));
 });
 
 gulp.task('lint', function() {
@@ -38,13 +38,18 @@ gulp.task('lint', function() {
 });
 
 gulp.task('serve', ['bundle'], function() {
-	gulp.src('.').pipe(webserver());
+  gulp.src('.').pipe(webserver());
 });
 
 gulp.task('test', function () {
-	return gulp.src('test/*.js').pipe(jasmine());
+  return gulp.src('test/*.js').pipe(jasmine());
 });
 
 gulp.task('test-travisci', function () {
-	return gulp.src('test/*.js').pipe(jasmine());
+  return gulp.src('test/*.js')
+    .pipe(cover.instrument({ pattern: ['lib/koara/**/*.js'] }))
+    .pipe(jasmine())
+    .pipe(cover.gather())
+    .pipe(cover.format({ reporter: 'lcov' }))
+    //.pipe(coveralls());
 });
